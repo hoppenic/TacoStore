a54cb5c329bb8dfc9de8c0bcea8abbe6bc383c73
hoppenic <hoppenic@gmail.com>
2018-08-13 12:15:45 -0500

    Add project files.

---
+++ TacoStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TacoStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

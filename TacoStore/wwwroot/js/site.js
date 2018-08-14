d12e40e8981b088e2d8c4d4918ce8b84c578fdf1
hoppenic <hoppenic@gmail.com>
2018-08-14 08:21:02 -0500

    updating, playing with jQuery

--- TacoStore/wwwroot/js/site.js
+++ TacoStore/wwwroot/js/site.js
@@ -1,1 +1,26 @@
 ﻿// Write your JavaScript code.
+
+//initialializing my jQuery
+$(document).ready(() => {
+
+    $("document").ready(function () {
+        $("#mainheader1").css("color","green")
+    })
+
+
+    $("document").ready(function () {
+        $("#indexp1").css("background","green")
+    })
+
+
+    $("document").ready(function () {
+        $("indexp2").css("color","20px")
+    })
+
+
+
+
+
+
+
+});

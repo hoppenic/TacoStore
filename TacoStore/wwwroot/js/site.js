7f7e29328a4f16f83e7109dfe6681c180824a4be
hoppenic <hoppenic@gmail.com>
2018-08-14 08:34:34 -0500

    update

--- TacoStore/wwwroot/js/site.js
+++ TacoStore/wwwroot/js/site.js
@@ -12,9 +12,9 @@ $(document).ready(() => {
         $("#indexp1").css("background","green")
     })
 
-
+    //no idea why this one is different syntax for changing only the font?
     $("document").ready(function () {
-        $("indexp2").css("color","20px")
+        $("#indexp2").css({ "color": "red"})
     })
 
 

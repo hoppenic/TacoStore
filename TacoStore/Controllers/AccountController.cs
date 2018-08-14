66bc2f4e0f90676c43482f1bb4e1b3535d88d55e
hoppenic <hoppenic@gmail.com>
2018-08-14 07:38:46 -0500

    update

---
+++ TacoStore/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TacoStore.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace TacoStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(string username, string password)
+        {
+            return Ok();
+        }
+
+
+
+
+
+
+    }
+}
---
+++ TacoStore/Models/RegisterViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TacoStore.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}

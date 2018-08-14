99afdcb47d03d387eea36f8c667b32f8e273f6f1
hoppenic <hoppenic@gmail.com>
2018-08-14 13:32:05 -0500

    DbContext

---
+++ TacoStore/Models/TacoStoreDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace TacoStore.Models
+{
+    public class TacoStoreDbContext :IdentityDbContext<TacoStoreUser>
+    {
+
+        public TacoStoreDbContext() : base()
+        {
+
+
+
+        }
+
+
+        public TacoStoreDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class TacoStoreUser:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+}
